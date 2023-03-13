6bb32245d3d5705ca302da37cf9dc877e10f306a
CarlosRuiz <carua97@gmail.com>
2023-03-13 17:02:13 +0100

    Add index and DB conections

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const a = 3;
-console.log(a);
+import createDebug from 'debug';
+import http from 'http';
+import { dbConnect } from './db/db.connect';
+
+const debug = createDebug('MM');
+
+const PORT = process.env.PORT || 4500;
+
+const server = http.createServer();
+
+dbConnect().then((mongoose) => {
+  server.listen(PORT);
+  debug('BD ', mongoose.connection.db.databaseName);
+});
