51b12141de3615e5df9f795c430e2826297a64b4
CarlosRuiz <carua97@gmail.com>
2023-03-13 18:22:45 +0100

    Modified index, add app to run server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import createDebug from 'debug';
 import http from 'http';
-import { dbConnect } from './db/db.connect';
+import { app } from './app.js';
+import { dbConnect } from './db/db.connect.js';
 
-const debug = createDebug('MM');
+const debug = createDebug('MM:Index');
 
 const PORT = process.env.PORT || 4500;
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 dbConnect().then((mongoose) => {
   server.listen(PORT);
   debug('BD ', mongoose.connection.db.databaseName);
+});
+
+server.on('listening', () => {
+  debug('Listening in http://localhost:' + PORT);
 });
