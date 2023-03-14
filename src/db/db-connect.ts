277913d71a21d5f5c5ddfb288694851708308e5e
CarlosRuiz <carua97@gmail.com>
2023-03-14 10:35:29 +0100

    Add db test file

--- src/db/db-connect.ts
+++ src/db/db-connect.ts
@@ -3,11 +3,12 @@ import { config } from './db-config.js';
 
 const { user, password, cluster, dbName } = config;
 
-export const dbConnect = () => {
+export const dbConnect = (env?: string) => {
+  const envCondition = env || process.env.NODE_ENV;
   const DBNameCondition =
     // The dbName is change here, not in .env -> for the supertest to work!
     // eslint-disable-next-line no-negated-condition
-    process.env.NODE_ENV !== 'test' ? dbName : 'testing_maze_manor';
+    envCondition !== 'test' ? dbName : 'testing_maze_manor';
   const uri = `mongodb+srv://${user}:${password}@${cluster}/${DBNameCondition}?retryWrites=true&w=majority`;
   return mongoose.connect(uri);
 };
