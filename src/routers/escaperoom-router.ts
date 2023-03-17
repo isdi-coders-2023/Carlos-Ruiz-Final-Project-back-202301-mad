f69c155455300192d9d15e0fe7f2bc2a7c87bda1
CarlosRuiz <carua97@gmail.com>
2023-03-17 13:19:59 +0100

    Add routers and app

--- src/routers/escaperoom-router.ts
+++ src/routers/escaperoom-router.ts
@@ -1,7 +1,7 @@
 import { Router as router } from 'express';
 import createDebug from 'debug';
-import { EscapeRoomMongoRepo } from '../repository/espaceroom/escaperoom-mongo-repo';
-import { EscapeRoomController } from '../controllers/escaperoom-controller';
+import { EscapeRoomMongoRepo } from '../repository/espaceroom/escaperoom-mongo-repo.js';
+import { EscapeRoomController } from '../controllers/escaperoom-controller.js';
 
 const debug = createDebug('MM:escaperoom:router');
 
