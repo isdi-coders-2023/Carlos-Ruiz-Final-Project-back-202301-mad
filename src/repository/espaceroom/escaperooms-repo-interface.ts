18ed2123b129c14bc34478492a0dbdff41877965
CarlosRuiz <carua97@gmail.com>
2023-03-20 15:57:03 +0100

    Modified readById interface

--- src/repository/espaceroom/escaperooms-repo-interface.ts
+++ src/repository/espaceroom/escaperooms-repo-interface.ts
@@ -4,6 +4,6 @@ export interface EscapeRoomRepo<T> {
   search(query: { key: string; value: unknown }): Promise<T[]>;
 
   read(): Promise<T[]>;
-  read(id: string): Promise<T>;
+  readById(id: string): Promise<T>;
   readFilter(theme: string): Promise<T[]>;
 }
