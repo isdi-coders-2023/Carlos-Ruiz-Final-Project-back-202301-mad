43850064fe2b06964eb836ffb943062261d73677
CarlosRuiz <carua97@gmail.com>
2023-03-21 18:56:28 +0100

    Add reservations mongo model

---
+++ src/repository/reservations/reservations-mongo.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from 'mongoose';
+import { Reservation } from '../../entities/reservation';
+
+const reservationSchema = new Schema<Reservation>({
+  reserveDate: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  escaperoom: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+reservationSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._v;
+    delete returnedObject._id;
+  },
+});
+
+export const UserModel = model(
+  'Reservation',
+  reservationSchema,
+  'reservations'
+);
