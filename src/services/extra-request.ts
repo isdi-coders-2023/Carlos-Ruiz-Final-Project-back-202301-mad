311694dd51d897ea6a06e19399226bc438bd7c6f
Carlos <carua97@gmail.com>
2023-03-25 16:00:12 +0100

    Move interface extra request and interface token put inside auth

--- src/services/extra-request.ts
+++
@@ -1,6 +0,0 @@
-import { PayloadToken } from './token-info';
-import { Request } from 'express';
-
-export interface ExtraRequest extends Request {
-  tokenInfo: PayloadToken;
-}
--- src/services/token-info.ts
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export interface PayloadToken extends jwt.JwtPayload {
-  id: string;
-  email: string;
-}
