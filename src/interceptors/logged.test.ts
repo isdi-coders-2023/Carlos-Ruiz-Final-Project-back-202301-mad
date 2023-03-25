650e7ba0547f24865aaa1f5b9f6e363aa342842d
Carlos <carua97@gmail.com>
2023-03-25 13:23:08 +0100

    Change interceptor file name for logged

--- src/interceptors/logged.test.ts
+++ src/interceptors/logged.test.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response } from 'express';
 import { Auth } from '../services/auth';
 import { ExtraRequest } from '../services/extra-request';
-import { Interceptors } from './interceptors';
+import { logged } from './logged';
 
 jest.mock('../services/auth');
 
@@ -17,21 +17,21 @@ describe('Given the interceptors class (LOGGED)', () => {
   describe('when !authHeaderInfo', () => {
     (mockReq.get as jest.Mock).mockReturnValue(undefined);
     test('then it should call next error', () => {
-      Interceptors.logged(mockReq, resp, next);
+      logged(mockReq, resp, next);
       expect(next).toHaveBeenCalled();
     });
   });
   describe('when authHeaderInfo not start with Bearer', () => {
     test('then it should call next error', () => {
       (mockReq.get as jest.Mock).mockReturnValue('Bear');
-      Interceptors.logged(mockReq, resp, next);
+      logged(mockReq, resp, next);
       expect(next).toHaveBeenCalled();
     });
   });
   describe('when authHeaderInfo is Ok', () => {
     test('then it should call the next function passing by the Auth verification', () => {
       (mockReq.get as jest.Mock).mockReturnValue('Bearer testToken');
-      Interceptors.logged(mockReq, resp, next);
+      logged(mockReq, resp, next);
       expect(next).toHaveBeenCalled();
     });
   });
