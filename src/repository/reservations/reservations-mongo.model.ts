ba41d5aae2937b1d0107d90f211763ab9a1c9465
Carlos <carua97@gmail.com>
2023-03-25 14:39:06 +0100

    Change model reservation to populate scaperooms

--- src/repository/reservations/reservations-mongo.model.ts
+++ src/repository/reservations/reservations-mongo.model.ts
@@ -10,10 +10,12 @@ const reservationSchema = new Schema<Reservation>({
     type: Schema.Types.ObjectId,
     required: true,
   },
-  escaperoom: {
-    type: Schema.Types.ObjectId,
-    required: true,
-  },
+  escaperoom: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Espaceroom',
+    },
+  ],
 });
 
 reservationSchema.set('toJSON', {
