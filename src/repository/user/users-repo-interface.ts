2c17b1bc001b330591c4ffe91eadce287f09a9d8
Carlos <carua97@gmail.com>
2023-03-25 20:43:29 +0100

    Add repo readId, interface and test

--- src/repository/user/users-repo-interface.ts
+++ src/repository/user/users-repo-interface.ts
@@ -1,5 +1,6 @@
 export interface UserRepo<T> {
   create(_info: Partial<T>): Promise<T>;
   search(query: { key: string; value: unknown }): Promise<T[]>;
+  readId(id: string): Promise<T>;
   update(user: Partial<T>): Promise<T>;
 }
